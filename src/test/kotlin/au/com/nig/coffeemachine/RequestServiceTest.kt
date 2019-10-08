f7db68b108612272aa789ed278829ca47ebcfb59
Nicolas Guignard <nicolas065@gmail.com>
2019-10-08 22:38:56 +1100

    new test with junit 5 (#5)
    * new test with junit 5

--- src/test/kotlin/au/com/nig/coffeemachine/RequestServiceTest.kt
+++ src/test/kotlin/au/com/nig/coffeemachine/RequestServiceTest.kt
@@ -1,6 +1,7 @@
 package au.com.nig.coffeemachine
 
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 import java.math.BigDecimal
@@ -33,3 +34,32 @@ object RequestServiceTest : Spek ({
         }
     }
 })
+
+internal class RequestServiceTest2 {
+    @Test
+    fun `when receiving an order of making a hot chocolate with sugars and stick and the right amount it should return everything right`() {
+        // GIVEN
+        val amount = BigDecimal.valueOf(0.6)
+        val drink = HotChocolate()
+
+        // WHEN
+        val result = RequestService.validateDrinkPayment(drink, amount)
+
+        // THEN
+        Assertions.assertEquals(drink, result)
+//        Assertions.assertTrue(false)
+    }
+    @Test
+    fun `when receiving aan order of making a hot chocolate AND NOT the right amount it should return a message telling the missing amount`() {
+        // GIVEN
+        val amount = BigDecimal.valueOf(0.4)
+        val drink = HotChocolate()
+
+        // WHEN
+        val result = RequestService.validateDrinkPayment(drink, amount) as Message
+
+        // THEN
+        Assertions.assertEquals("Your order is missing $0.1", result.msg)
+//        Assertions.assertTrue(false)
+    }
+}
