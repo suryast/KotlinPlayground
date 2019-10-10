d3c9553a73e9b74c69779e1225421bf3900b1d2d
nicolas <nicolas065@gmail.com>
2019-10-10 16:08:47 +1100

    start of pipeline refactor

---
+++ src/test/kotlin/au/com/nig/pipeline/AuthorTest.kt
@@ -0,0 +1,22 @@
+package au.com.nig.pipeline
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class AuthorTest {
+    @Test
+    fun `make sure it still works`() {
+        // Given
+        val a0 = Author("nig", "stuff", "comp1")
+        val a1 = Author("apha", "stiff", "comp1")
+        val a2 = Author("npa", "blah", "comp2")
+        val authors = listOf(a0, a1, a2)
+        val expectedResults = listOf("stuff", "stiff")
+
+        // When
+        val result = Author.twitterHandles(authors, "comp1")
+
+        // Then
+        assertEquals(expectedResults, result)
+    }
+}
