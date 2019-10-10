9ad5a60a4d2f3a6b9ccdf79e5da6a4465a3a7a0a
nicolas <nicolas065@gmail.com>
2019-10-10 16:14:29 +1100

    first change

--- src/main/kotlin/au/com/nig/pipeline/Author.kt
+++ src/main/kotlin/au/com/nig/pipeline/Author.kt
@@ -4,12 +4,12 @@ class Author(val name: String, val twitterHandle: String, val company: String) {
     companion object {
         fun twitterHandles(authors: Iterable<Author>, company: String): Iterable<String> {
             var result: MutableList<String> = mutableListOf()
-            for (a in authors) {
-                if (a.company == company) {
-                    var handle = a.twitterHandle
-                    if (handle != null)
-                        result.add(handle)
-                }
+            var iterableAuthors = authors
+                .filter { it.company.equals(company) }
+            for (a in iterableAuthors) {
+                var handle = a.twitterHandle
+                if (handle != null)
+                    result.add(handle)
             }
             return result
         }
