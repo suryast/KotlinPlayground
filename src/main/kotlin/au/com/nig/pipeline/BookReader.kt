72758d0d37524e212c03e2c39cd0cff8af8b5cfd
nicolas <nicolas065@gmail.com>
2019-10-10 22:18:10 +1100

    last refactor

--- src/main/kotlin/au/com/nig/pipeline/BookReader.kt
+++ src/main/kotlin/au/com/nig/pipeline/BookReader.kt
@@ -1,22 +1,16 @@
 package au.com.nig.pipeline
 
 import java.util.*
-import kotlin.collections.HashSet
 
 class BookReader(private val dataService: DataService) {
 
     fun getReadersOfBooks(readers: Collection<String>, books: Collection<String>, date: Date): Set<String> {
-        val result: MutableSet<String> = HashSet()
         val data: Map<String, Collection<String>> = dataService.getBooksReadOn(date)
-        val entries = data.entries
+        return data.entries
             .filter { readers.contains(it.key) }
             .filter { books.intersect(it.value).isNotEmpty() }
+            .map { it.key }
             .toHashSet()
-        for (e in entries) {
-            for (bookId in books) {
-                result.add(e.key)
-            }
-        }
-        return result
+
     }
 }
